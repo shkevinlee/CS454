b790ec445598ca1a694dc142ad5491138d7f4f4f
hiall21 <hiall21@kaist.ac.kr>
2018-11-30 14:54:49 +0900

    ILP-timelimit added

--- ILPAlgorithm/ilp.py
+++ ILPAlgorithm/ilp.py
@@ -1,30 +1,30 @@
 from pulp import *
+from gurobipy import *
 import pandas as pd
 import os
 DATA_DIR = './data'
-df = pd.read_pickle(os.path.join(DATA_DIR, 'bugzilla_firefox_2017_random01.pkl'))
+df = pd.read_pickle(os.path.join(DATA_DIR, 'bugzilla_eclipse_log(comments)_2016meancost.pkl'))
 cnt = 0
-print(df.values[260:300])
-print(len(df))
-idx = [x for x in range(10)]
+#print(len(df))
+idx = [x for x in range(len(df))]
 prob = LpProblem("term_project", LpMaximize)
 
 answer = LpVariable.dicts("answer",idx,0,1,LpInteger)
 
 prob += lpSum([df.values[i][4]*answer[i] for i in idx])
 
-prob += lpSum([df.values[j][3]*answer[j] for j in idx]) <= 15
+prob += lpSum([df.values[j][3]*answer[j] for j in idx]) <= 50700
 
-status = prob.solve()
+status = prob.solve(GUROBI(timeLimit=0.03))
 
 
-print(LpStatus[status])
+#print(LpStatus[status])
 
-for v in prob.variables():
-	print (v.name, " = ", v.varValue)
+#for v in prob.variables():
+#	print (v.name, " = ", v.varValue)
 
-for w in prob.variables():
-	if (w.varValue == 1.0):
-		print (w.name, " = ", df.values[cnt][0])
-	cnt += 1
+# for w in prob.variables():
+# 	if (w.varValue == 1.0):
+# 		print (w.name, " = ", df.values[cnt][0])
+# 	cnt += 1
 print("Maximum Profit = ", value(prob.objective))
